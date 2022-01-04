9593f0ccf2985cea5dc728fb234217208cd8e732
Matheus Alves <matheus.aalves@hotmail.com>
2022-01-04 13:44:26 -0300

    Requisito 5 dos infernos

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import PlanetCard from './PlanetCard';
 import Title from './Title';
+
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        <PlanetCard />
       </div>
     );
   }
