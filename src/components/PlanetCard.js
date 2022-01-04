78c54683e7ec6d07bc6b208e3273186110f9198a
Matheus Alves <matheus.aalves@hotmail.com>
2022-01-04 14:00:27 -0300

    VAI AVALIADOR

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -7,7 +7,7 @@ class PlanetCard extends React.Component {
     const { planetImage } = this.props;
     return (
       <div data-testid="planet-card">
-        <h3 data-testid="planet-name">{planetName}</h3>
+        <h3 data-testid="planet-name">{planetName }</h3>
         <img src={ planetImage } alt={ `Planeta ${planetName}!` } />
       </div>
 
